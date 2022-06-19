19672a65988dbab63541b5f7f0e9e99293f9ead4
Mohammed Ashique <mohammedashique127@gmail.com>
2022-06-19 07:55:01 +0530

    ashique

---
+++ comp/navbar.js
@@ -0,0 +1,24 @@
+let navbar = ()=>{
+
+    return `<img src="https://www2.skillsoft.com/wp-content/uploads/2019/10/Deccan-Chronicle.png"  id="aslogo" alt="logo">
+    <div id="time">
+    </div>
+    <hr class="line">
+    <div id="asnavmenu">
+      
+   <a href="./index.html"> <img src="https://cdn-icons-png.flaticon.com/512/25/25694.png" alt="home icon" id="home-icon"></a>
+      <a href="./nation.html">Nation</a>
+      <a href="./world.html">World</a>
+      <a href="./entertainment.html">Entertainment</a>
+      <a href="./job.html">Jobs & Education</a>
+      <a href="./sports.html">Sports</a>
+      <a href="./technology.html">Technology</a>
+      <a href="./lifestyle.html">Lifestyle</a>
+      <a href="#">Gallery</a>
+      <a href="/">More...</a>
+    </div>
+    <hr>`
+ 
+}
+
+export {navbar}
